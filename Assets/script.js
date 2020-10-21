fb179b198f0dc5f78942a4b20d195dbbbc93403f
Logan Morro <loganfmorro@gmail.com>
2020-10-21 19:34:52 -0400

    updated theme and portfolio pages

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+// on click events for portfolio images
+
+$(document).ready(function() {
+
+    $("#stockUpIMG").on('click', function(){
+        window.location = "https://bohdicave.github.io/StockUp/"; 
+    });   
+});
